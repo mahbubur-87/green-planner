62ec00b61346994f73fcf179ef028777ef345af3
Mahbubur Rahman <mahbubur@impact4good.eu>
2022-10-16 00:54:39 +0200

    update serializer codes

--- green/serializers.py
+++ green/serializers.py
@@ -39,8 +39,8 @@ class PackageSerializer(serializers.ModelSerializer):
     location_from = serializers.CharField(max_length=200)
     location_to = serializers.CharField(max_length=200)
     package_rank = serializers.IntegerField(required=False, default=1)
-    route = serializers.serializers.PrimaryKeyRelatedField(read_only=True)
-    stay = serializers.serializers.PrimaryKeyRelatedField(read_only=True)
+    route = serializers.PrimaryKeyRelatedField(read_only=True)
+    stay = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Package
