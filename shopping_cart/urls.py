1c0492217246f4d2f41b08f634e6ec8baa5f617d
Mahbubur Rahman <mahbubur@impact4good.eu>
2022-10-15 12:07:12 +0200

    change url path for shopping cart

--- shopping_cart/urls.py
+++ shopping_cart/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import CartItemViews
 
 urlpatterns = [
-    path('cart-items/', CartItemViews.as_view()),
+    #path('cart-items/', CartItemViews.as_view()),
+    path('', CartItemViews.as_view()),
     path('cart-items/<int:id>', CartItemViews.as_view())
 ]
