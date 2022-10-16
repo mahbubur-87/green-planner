e886469446e97b32f35d581b31cdeae9648ae4f5
Mahbubur Rahman <mahbubur@impact4good.eu>
2022-10-16 02:44:46 +0200

    update new url in green package

--- green/views.py
+++ green/views.py
@@ -94,11 +94,10 @@ class PackageViews(APIView):
 
     def get(self, request, location_from=None, location_to=None):
         if location_from and location_to:
-            item = Package.objects.filter(location_from=location_from).filter(location_to=location_to)
-            serializer = PackageSerializer(item)
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-
-        items = Package.objects.all()
+            items = Package.objects.filter(location_from=location_from).filter(location_to=location_to)
+        else:
+            items = Package.objects.all()
+            
         serializer = PackageSerializer(items, many=True)
         return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)    
         
